1409916971d270c2b672e2914302e9959267b43e
Sholehulhudaa <sholehulhuda@gmail.com>
2019-11-21 12:17:06 +0700

    first commit

---
+++ tutorialss3/app/src/main/java/com/example/tutorialss3/ActivityInputOutput.java
@@ -0,0 +1,38 @@
+package com.example.tutorialss3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.tutorialss3.ActivityProses;
+import com.example.tutorialss3.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityInputOutput extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_io);
+        final EditText firstNum = (EditText)findViewById(R.id.angkaSatu);
+        final EditText secNum = (EditText)findViewById(R.id.angkaDua);
+        Button btnAdd = (Button)findViewById(R.id.btnTambah);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// Memberi nilai ke dua buah variabel yakni num1 dan num2
+// Nilai diambil dari nilai yang dimasukkan pengguna di TextField
+                int num1 = Integer.parseInt(firstNum.getText().toString());
+                int num2 = Integer.parseInt(secNum.getText().toString());
+// Membuat objek Intent
+                Intent intent = new Intent(ActivityInputOutput.this, ActivityProses.class);
+
+                intent.putExtra("jumlah",num1+" + "+num2+" = "+(num1+num2));
+// Menjalankan activity dengan parameter Intent
+                startActivity(intent);
+            }
+        });
+    }
+}
